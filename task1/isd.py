5c9095067f5bc224bc8c8b026e5b06bcbc6d97e0
Tina Topka <anon.justch1ll1n@gmail.com>
2025-02-08 01:59:07 +0200

    isd works yay

--- task1/isd.py
+++ task1/isd.py
@@ -1,8 +1,11 @@
 import numpy as np
 from prettytable import PrettyTable
-from .poly_plot import plot
 
 table = PrettyTable()
+
+
+def check(x, num1, num2):
+    return (x > num1 and x < num2)
 
 
 def isd_calc(values, counts, n):
@@ -10,18 +13,11 @@ def isd_calc(values, counts, n):
 
     k = round(np.sqrt(n))
     print(k)
-    h = (max(values) - min(values))/k
+    h = round((max(values) - min(values))/k, 3)
     print(h)
 
-    data = np.array([])
-    count = 0
-    curr_value = round(values[0], 2)
-    while count < k:
-        np.append(data, round(curr_value, 2))
-        count += 1
-        curr_value += h
-
-    if (curr_value+h) < values[np.size(values)-1]:
+    data = np.array([round((values[0] + i * h), 2) for i in range(k+1)])
+    if data[-1] < values[-1]:
         check_last_val = False
     print(data)
 
@@ -29,45 +25,45 @@ def isd_calc(values, counts, n):
     num = 0
     saved = 0
     flag = 0
-    for i in range(len(data)-1):
-        for j in range(flag, np.size(counts)-1):
-            if counts[j] > data[i] and counts[j] < data[i+1]:
+    count = 0
+    
+    for i in range(np.size(data)-1):
+        for j in range(flag, np.size(values)):
+            if check(values[j], data[i], data[i+1]) or j == 0 or j == np.size(values)-1:
                 num += counts[j]
                 continue
-            else:
+            elif values[j] == data[i] or values[j] == data[i+1]:
                 j1 = round(counts[j]/2)
                 j2 = counts[j] - j1
-                if j1 < j2:
-                    num += j1
-                    saved = j2
-                else:
-                    num += j2
-                    saved = j1
-            np.append(n_data, num)
-            num = saved
-            saved *= 0
-            flag = j
+                saved = max(j1, j2)
+                num += min(j1, j2)
+                flag = j+1
+            else:
+                flag = j
+            break
+        n_data = np.append(n_data, num)
+        num *= 0
+        num += saved
+        saved *= 0
     print(n_data)
 
     w_div_h = []
     for i in n_data:
-        w_div_h.append(i/(n*h))
-    
-    cols = round(len(data)/2)
-    data2d = data.reshape(cols, 2)
+        w_div_h.append(round(i/(n*h), 3))
 
-    table_print(data2d, n_data, w_div_h, check_last_val)
+    table_print(data, n_data, w_div_h, check_last_val)
     #plot(values, w)
 
 
 def table_print(values, counts, w, check_last_val):
     table.field_names = ["Інтервали", "n*_i", "w*_i / h"]
-    for i in range(np.size(counts)):
+    for i in range(np.size(counts-1)):
         if i == 0:
-            table.add_row([f"[{values[i][0]}; {values[i][1]})", counts[i], w[i]], divider=True)
-        elif i == np.size(values)-1:
-            table.add_row([f"[{values[i][0]}; {values[i][1]}{']' if check_last_val else ')'}", counts[i], w[i]],
+            table.add_row([f"[{values[i]}; {values[i+1]})", counts[i], w[i]], divider=True)
+        elif i == np.size(counts-2):
+            table.add_row([f"[{values[i]}; {values[i+1]}{']' if check_last_val else ')'}", counts[i], w[i]],
                           divider=True)
+            break
         else:
-            table.add_row([f"({values[i][0]}; {values[i][1]})", counts[i], w[i]], divider=True)
+            table.add_row([f"({values[i]}; {values[i+1]})", counts[i], w[i]], divider=True)
     print(table)
--- task1/task1.py
+++ task1/task1.py
@@ -8,16 +8,22 @@ data = np.array([12, 15, 7, 19, 15, 27, 22, 4,
                  11, 19, 15, 7, 1, 12, 22, 4,
                  11, 15, 19, 12, 7, 27, 15, 19,
                  11, 22, 12, 19, 15, 12, 15, 31])
+
+# data = np.array([0.87, 0.94, 0.99, 0.90, 0.90, 0.87, 0.85, 0.87,
+#                  0.90, 0.94, 0.87, 0.87, 0.82, 0.90, 0.94, 0.90,
+#                  0.85, 0.85, 0.87, 0.94, 0.81, 0.82, 0.87, 0.97,
+#                  0.90, 0.94, 0.85, 0.81, 0.87, 0.85, 0.90, 0.82,
+#                  0.99, 0.90, 0.94, 0.82, 0.97, 0.81, 0.85, 0.87])
 var_arr = np.sort(data)
-n = 32
+n = np.size(data)
 
 
 def start():
     values, counts = np.unique(data, return_counts=True)
 
     w = []
-    for i in values:
+    for i in counts:
         w.append(i/n)
 
-    # dsd_calc(values, counts, w)
+    #dsd_calc(values, counts, w)
     isd_calc(values, counts, n)
